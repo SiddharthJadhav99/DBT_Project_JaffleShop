a769437b036dc118ac9508ab8a1daeb2c6289f0d
Siddharth Jadhav <jadhavsid99@gmail.com>
2022-04-08 12:09:27 +0530

    Added File Upload and Auto DBT run

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import json
-import logging
-import multiprocessing 
 import os
-import re
-import sys
-import time
-from datetime import datetime
+import argparse
 
-import numpy as np
-import pandas as pd
 
-# os.system("bq load --autodetect  dbt_fundamentals.orders gs://training_freshers/SiddharthJadhav/orders.csv")
-# os.system("bq load --autodetect  dbt_fundamentals.customers gs://training_freshers/SiddharthJadhav/users.csv")
-# os.system("bq load --autodetect  dbt_fundamentals.payments gs://training_freshers/SiddharthJadhav/payments.csv")
-os.system("pushd D:\DBT_Fundamentals\dbt_project & dbt run --full-refresh")
+with open("D:\\DBT_Fundamentals\\dbt_project\\data.csv") as file:
+
+    for element in file:
+        dest, format, sourc = element.split(',')
+        cmnd = f"bq load --autodetect --replace --source_format={format} {dest} {sourc} "
+        print(cmnd)
+        os.system(cmnd)
+
+parser = argparse.ArgumentParser(
+    description="DBT Fundamentals Project"
+)
+
+parser.add_argument(
+    "--dbtpath", type=str, help="Path of dbt Project"
+)
+
+args = parser.parse_args()
+
+os.system(f"pushd {args.dbtpath} & dbt run --full-refresh")
